c49ce5cb395948ab75b81b6f48a7a0aaa1f18f85
whereismydamnhorsebilly <wavesandpurplehaze@gmail.com>
2024-09-11 19:04:00 +0300

    Initial commit

---
+++ src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello world</h1>");
+    }
+}
